2c8bb07384ee8c011ac24739a5185cbb71a1c92c
MaximeLedru <55274158+MaximeLedru@users.noreply.github.com>
2020-01-28 13:58:11 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,49 +1,59 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.login(process.env.TOKEN);
-var parler = true; 
+var parler = true;
+const ms = require("ms");
+
+
+const Bot = new Discord.Client()
+
+
 
 bot.on('ready', () => {
     bot.user.setActivity("Traduit le langage Nyarlathotep");
     console.log('Le bot est prêt pour être utilisé !');
 
 });
-bot.on('message', message => {
-    if (parler == true) {
-    if (message.content === '.') {
-        message.react("👍")
-        message.reply('*Cela veut dire : Content*');
-        }
-    }
-});
-client.on('message', message => {
-   if (message.content === '.clear') {
-       
-      message.delete(1000); //Supposed to delete message
-      message.reply('Messages supprimés maître');
-   }
-});
+
+
 bot.on('message', message => {
 
-        if (message.content === '.help') {
+        if (message.content === '.clear') {
             message.react("👍")
-            message.author.send('Voici mes commandes maître : \n***Ta gueule DotDictionnary*** : je ferme ma bouche \n ***Parle DotDictionnary***: J ouvre ma bouche');
+            message.channel.bulkDelete(100);
+            message.reply('Les messages ont été supprimé maître !');
         
     }
 });
 bot.on('message', message => {
-    if (parler == true) {
-    if (message.content === '..') {
+
+    if (message.content === '.info') {
         message.react("👍")
-        message.reply('*Cela veut dire : je m en fou*');
+        message.channel.send('```Mon créateur : Wukimos#0762\n Ma version ! 1.0.1 \nMes fonctions : Je joue de la musique, je traduit le langage . , et je répond à tout mes maîtres.```');
+
+    }
+});
+bot.on('message', message => {
+
+    if (message.content === '.help') {
+        message.react("👍")
+        message.author.send('Voici mes commandes maître : \n***Ta gueule DotDictionnary*** : je ferme ma bouche \n ***Parle DotDictionnary***: J ouvre ma bouche');
+
+    }
+});
+bot.on('message', message => {
+    if (parler == true) {
+        if (message.content === '..') {
+            message.react("👍")
+            message.reply('*Cela veut dire : je m en fou*');
         }
     }
 });
 bot.on('message', message => {
     if (parler == true) {
-    if (message.content === '....') {
-        message.react("👍")
-        message.reply("*Cela veut dire : Suicide*");
+        if (message.content === '....') {
+            message.react("👍")
+            message.reply("*Cela veut dire : Suicide*");
         }
     }
 });
@@ -57,17 +67,17 @@ bot.on('message', message => {
 });
 bot.on('message', message => {
 
-        if (message.content === 'ta gueule DotDictionnary') {
-            message.react("👍")
-            message.reply("D'accord maître, je ferme ma gueule :)");
-            bot.user.setActivity("Je ferme ma gueule");
-            parler = false;
-        }
- 
+    if (message.content === 'ta gueule Esclave' || message.content === 'ta gueule esclave'  ) {
+        message.react("👍")
+        message.reply("D'accord maître, je ferme ma gueule :)");
+        bot.user.setActivity("Je ferme ma gueule");
+        parler = false;
+    }
+
 });
 bot.on('message', message => {
 
-    if (message.content === 'parle DotDictionnary') {
+    if (message.content === 'parle Esclave' || message.content === 'parle esclave') {
         message.react("👍")
         bot.user.setActivity("Traduit le langage Nyarlathotep");
         message.reply("D'accord maître suprême :)");
@@ -75,3 +85,14 @@ bot.on('message', message => {
     }
 
 });
+//Message de bienvenue
+
+bot.on('guildMemberAdd', member => {
+    member.guild.channels.get('671687716622303251').send("Bienvenue sur le serveur " + member+" ! \n\n\n pour connaître mes commandes, il faut que tu fasse **.help**");
+});
+
+bot.on('guildMemberRemove', member => {
+    member.guild.channels.get('671687716622303251').send(member + " a quitté le royaume.. Dommage pour lui :'(");
+});
+//commandes admin
+
